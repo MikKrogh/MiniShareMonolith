8c74623e65eeb544feef11f01ba851532047c762
Mikael Vestergaard Krogh <mikkro89@outlook.dk>
2024-12-11 13:06:33 +0100

    updateing a few nuggets. issue with loading csproj file for modules, re-added them to project

--- Modules/PostsModule.Tests/PostRequest.cs
+++ Modules/PostsModule.Tests/PostRequest.cs
@@ -2,10 +2,10 @@
 
 internal class PostRequest
 {
-	public string? CreatorId { get; set; }
-	public string? Title { get; set; }
-	public string? Description { get; set; }
+	public string CreatorId { get; set; }
+	public string Title { get; set; }
+	public string Description { get; set; }
 
-	public string? PrimaryColor { get; set; }
-	public string? SecondaryColor { get; set; }
+	public string PrimaryColor { get; set; }
+	public string SecondaryColor { get; set; }
 }
---
+++ Modules/PostsModule/Infrastructure/PostsRepository.cs
@@ -0,0 +1,16 @@
+﻿using PostsModule.Domain;
+
+namespace PostsModule.Infrastructure;
+
+public class PostsRepository : IPostsRepository
+{
+    public Task Delete(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Save(Post post)
+    {
+        throw new NotImplementedException();
+    }
+}
