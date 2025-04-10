8830bc94703d94f1900b6e5d65c53b6d1671c928
Mikael Vestergaard Krogh <mikkro89@outlook.dk>
2025-04-10 20:41:34 +0200

    bn

--- Modules/PostsModule/Presentation/Endpoints/CreatePost.cs
+++ Modules/PostsModule/Presentation/Endpoints/CreatePost.cs
@@ -7,50 +7,51 @@ using PostsModule.Domain.Auth;
 namespace PostsModule.Presentation.Endpoints;
 public class CreatePost
 {
-    public static async Task<IResult> Process(ILogger<CreatePost>? logger,[FromServices] IRequestClient<CreatePostCommand> client, IAuthHelper auth, [FromBody] CreateBody body)
+    //public static async Task<IResult> Process(ILogger<CreatePost>? logger,[FromServices] IRequestClient<CreatePostCommand> client, IAuthHelper auth, [FromBody] CreateBody body)
+    public static async Task<IResult> Process()
     {
         return Results.Ok();
-        if (logger is null)
-        {
-            return Results.Problem("Logger is null");
-        }
-        logger.LogError("hello");
-        logger?.LogInformation("Creating post with title {title} and creatorId {creatorId}", body.Title, body.CreatorId);
-        var command = new CreatePostCommand()
-        {
-            Title = body.Title,
-            FactionName = body.FactionName,
-            Description = body.Description,
-            CreatorId = body.CreatorId,
-            PrimaryColor = string.IsNullOrEmpty(body.PrimaryColor) ? string.Empty : body.PrimaryColor,
-            SecondaryColor = string.IsNullOrEmpty(body.SecondaryColor) ? string.Empty : body.SecondaryColor,
-        };
+        //if (logger is null)
+        //{
+        //    return Results.Problem("Logger is null");
+        //}
+        //logger.LogError("hello");
+        //logger?.LogInformation("Creating post with title {title} and creatorId {creatorId}", body.Title, body.CreatorId);
+        //var command = new CreatePostCommand()
+        //{
+        //    Title = body.Title,
+        //    FactionName = body.FactionName,
+        //    Description = body.Description,
+        //    CreatorId = body.CreatorId,
+        //    PrimaryColor = string.IsNullOrEmpty(body.PrimaryColor) ? string.Empty : body.PrimaryColor,
+        //    SecondaryColor = string.IsNullOrEmpty(body.SecondaryColor) ? string.Empty : body.SecondaryColor,
+        //};
 
-        try
-        {
-            var clientResponse = await client.GetResponse<CommandResult<CreatePostCommandResult>>(command);
-            var commandResult = clientResponse.Message;
+        //try
+        //{
+        //    var clientResponse = await client.GetResponse<CommandResult<CreatePostCommandResult>>(command);
+        //    var commandResult = clientResponse.Message;
 
-            if (clientResponse.Message.IsSuccess && commandResult is not null)
-            {
-                var token = auth.CreateToken(
-                    DateTime.UtcNow.AddMinutes(5),
-                    ClaimValueHolder.Create("postId", commandResult.ResultValue.PostId)
-                );
+        //    if (clientResponse.Message.IsSuccess && commandResult is not null)
+        //    {
+        //        var token = auth.CreateToken(
+        //            DateTime.UtcNow.AddMinutes(5),
+        //            ClaimValueHolder.Create("postId", commandResult.ResultValue.PostId)
+        //        );
 
-                return Results.Ok(new SuccessRespnse
-                {
-                    PostId = commandResult.ResultValue.PostId,
-                    Token = token
-                });
-            }
-            return Results.StatusCode(409);
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, "Error creating post with title {title} and creatorId {creatorId}", body.Title, body.CreatorId);
-            return Results.Problem(ex.Message);
-        }
+        //        return Results.Ok(new SuccessRespnse
+        //        {
+        //            PostId = commandResult.ResultValue.PostId,
+        //            Token = token
+        //        });
+        //    }
+        //    return Results.StatusCode(409);
+        //}
+        //catch (Exception ex)
+        //{
+        //    logger.LogError(ex, "Error creating post with title {title} and creatorId {creatorId}", body.Title, body.CreatorId);
+        //    return Results.Problem(ex.Message);
+        //}
     }
 }
 public class SuccessRespnse
