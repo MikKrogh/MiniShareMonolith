8742403c2987317dd62c2fcf1523632a079fe3f8
Mikael Vestergaard Krogh <mikkro89@outlook.dk>
2025-06-21 11:40:27 +0200

    attempt to reduce calls for migrations

--- Modules/EngagementModule/EngagementDbContext.cs
+++ Modules/EngagementModule/EngagementDbContext.cs
@@ -17,8 +17,12 @@ public class EngagementDbContext : DbContext, IPostLikeService
 
         if (env.IsDevelopment() || env.IsEnvironment("Test"))
         {
-            Database.Migrate();
-            SaveChanges();
+            if (!Database.EnsureCreated()) {                 // If the database is not created, we apply migrations
+                Database.Migrate();
+                SaveChanges();
+            }
+
+
         }
     }
 
