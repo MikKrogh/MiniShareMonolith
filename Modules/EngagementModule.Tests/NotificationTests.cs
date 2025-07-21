17b7152f3332c5a645258608c7f2bf3e1fd97091
Mikael Vestergaard Krogh <mikkro89@outlook.dk>
2025-07-21 22:57:33 +0200

    æ

--- Modules/EngagementModule.Tests/NotificationTests.cs
+++ Modules/EngagementModule.Tests/NotificationTests.cs
@@ -22,7 +22,7 @@ public class NotificationTests : IClassFixture<EngagementWebApplication>
         string postId = Guid.NewGuid().ToString();
         string creatorId = Guid.NewGuid().ToString();
         await _factory.PublishPostCreatedEvent(postId, creatorId);
-        var t = await client.PostAsync($"/Engagement/notifications?userId={creatorId}", null);
+        await client.PostAsync($"/Engagement/notifications?userId={creatorId}", null);
 
         string commentatorId = Guid.NewGuid().ToString();
         var comment = new { Content = "This is a test comment." };
@@ -34,9 +34,30 @@ public class NotificationTests : IClassFixture<EngagementWebApplication>
         Assert.NotNull(notificationResponse);
         Assert.Single(notificationResponse);
         Assert.Equal(postId, notificationResponse[0].PostId);
+    }
 
+    [Fact]
+    public async Task GivenPostExistsWithCommentAndCreatorHasSyncedAfterCommentCreation_WhenUserChecksForNoticiation_ThenEmptySuccessIsReturned()
+    {
+        string postId = Guid.NewGuid().ToString();
+        string creatorId = Guid.NewGuid().ToString();
+        await _factory.PublishPostCreatedEvent(postId, creatorId);
+        await client.PostAsync($"/Engagement/notifications?userId={creatorId}", null);
 
+        string commentatorId = Guid.NewGuid().ToString();
+        var comment = new { Content = "This is a test comment." };
+        await client.PostAsJsonAsync($"/Engagement/{postId}/comments?userId={commentatorId}", comment);
+        var firstFetch = await client.GetFromJsonAsync<List<NotifocationsDto>>($"/Engagement/Notifications?userId={creatorId}");
 
+        await client.PostAsync($"/Engagement/notifications?userId={creatorId}", null);
+        //When
+        var secondFetch = await client.GetFromJsonAsync<List<NotifocationsDto>>($"/Engagement/Notifications?userId={creatorId}");
+        // Then
+        Assert.NotNull(firstFetch);
+        Assert.Single(firstFetch);
+        Assert.NotNull(secondFetch);
+        Assert.Empty(secondFetch);
 
     }
+
 }
