05c9b861a2663df0467935ea9c31002c2671bec8
Mikael Vestergaard Krogh <mikkro89@outlook.dk>
2025-10-14 21:24:13 +0200

    remoed the db entity that represents an image. and applied a new migration for this.

--- Modules/PostsModule/Migrations/20251014192247_remove-images-entities.Designer.cs
+++ Modules/PostsModule/Migrations/20251014192247_remove-images-entities.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PostsModule.Infrastructure;
@@ -11,9 +12,11 @@ using PostsModule.Infrastructure;
 namespace PostsModule.Migrations
 {
     [DbContext(typeof(PostsContext))]
-    partial class PostsContextModelSnapshot : ModelSnapshot
+    [Migration("20251014192247_remove-images-entities")]
+    partial class removeimagesentities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
