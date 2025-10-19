b21abaca431fa7188fdba36c451f1bb4c2299b6d
Mikael Vestergaard Krogh <mikkro89@outlook.dk>
2025-10-19 17:50:29 +0200

    global ecxcxception handler

--- MiniShare.Web/Program.cs
+++ MiniShare.Web/Program.cs
@@ -1,28 +1,46 @@
+using BarebonesMessageBroker;
+using EngagementModule;
+using OpenTelemetry.Metrics;
 using PostsModule;
 using PostsModule.Presentation;
-using UserModule;
-using EngagementModule;
-using BarebonesMessageBroker;
 using Prometheus;
+using UserModule;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 
 builder.Services.AddPostModuleServices(builder.Configuration);
 builder.Services.AddEngagementModuleServices(builder.Configuration);
 builder.Services.AddUserModuleServices(builder.Configuration);
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(metrics =>
+    {
+        metrics
+            .AddAspNetCoreInstrumentation();
+    });
+
 builder.Services.AddSingleton<BarebonesMessageBroker.IBus>(sp =>
 {
     var scopeFactory = sp.GetRequiredService<IServiceScopeFactory>();
     return new BareBonesBus(scopeFactory);
 });
+
 builder.Logging.SetMinimumLevel(LogLevel.Error);
 builder.Services.AddLogging();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
+
+
+app.UseExceptionHandler(errorApp =>
+{
+    errorApp.Run(async context =>
+    {
+        context.Response.StatusCode = 500;
+        context.Response.ContentType = "application/json";      
+    });
+});
 
 app.UseMetricServer();
 app.UseSwagger();
